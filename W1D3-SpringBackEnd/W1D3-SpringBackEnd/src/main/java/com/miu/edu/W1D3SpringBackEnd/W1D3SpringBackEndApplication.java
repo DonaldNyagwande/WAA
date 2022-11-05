86e4c17d9f9d7f6de0873f94ab205c574497c73f
DonaldNyagwande <112922823+DonaldNyagwande@users.noreply.github.com>
2022-11-05 13:56:01 -0500

    Initial Commit

---
+++ W1D3-SpringBackEnd/W1D3-SpringBackEnd/src/main/java/com/miu/edu/W1D3SpringBackEnd/W1D3SpringBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.miu.edu.W1D3SpringBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W1D3SpringBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W1D3SpringBackEndApplication.class, args);
+	}
+
+}
---
+++ W1D3-SpringBackEnd/W1D3-SpringBackEnd/src/test/java/com/miu/edu/W1D3SpringBackEnd/W1D3SpringBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miu.edu.W1D3SpringBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W1D3SpringBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
