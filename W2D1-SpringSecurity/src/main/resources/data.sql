b9d89a6af88979ae5ecc540472d93ca01e575434
DonaldNyagwande <112922823+DonaldNyagwande@users.noreply.github.com>
2022-11-11 15:52:02 -0600

    Update

--- W2D1-SpringSecurity/src/main/resources/data.sql
+++ W2D1-SpringSecurity/src/main/resources/data.sql
@@ -14,6 +14,13 @@ VALUES (1, 'ADMIN');
 INSERT INTO role (id, role)
 VALUES (2, 'USER');
 
+INSERT INTO users_roles (user_id, roles_id)
+VALUES (1, 1);
+INSERT INTO users_roles (user_id, roles_id)
+VALUES (2, 2);
+INSERT INTO users_roles (user_id, roles_id)
+VALUES (3, 2);
+
 
 -- POSTS
 
